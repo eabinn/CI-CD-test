dc3d246505b1c8301b7d731cf589c289e0d96065
eabinn <iby2455@gmail.com>
2023-04-10 14:24:29 +0900

    동일한 환경으로 작업 진행하기 위해 코드 수정

--- front/apps/main-app/next.config.js
+++ front/apps/main-app/next.config.js
@@ -1,10 +1,15 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   basePath: "/app",
   distDir: "build",
-  ...(process.env.NODE_ENV === "production" && { output: "standalone" }),
+  ...(isProd && {
+    output: "standalone",
+    assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
+  }),
 };
 
 module.exports = nextConfig;
